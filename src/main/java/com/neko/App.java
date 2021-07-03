601004444d7488f801d3b506b646a5483c8ed6a4
SolarisNeko <1417015340@qq.com>
2021-07-04 00:25:48 +0800

    engineer

---
+++ src/main/java/com/neko/App.java
@@ -0,0 +1,13 @@
+package com.neko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
