ac1bcb07d5c1dfff2f36cf83b212386ff181d844
SolarisNeko <1417015340@qq.com>
2021-07-07 23:36:34 +0800

    测试,注释

--- src/main/java/com/neko/entity/Student.java
+++ src/main/java/com/neko/entity/Student.java
@@ -1,27 +1,27 @@
-package com.neko.entity;
-
-import lombok.Data;
-
-import java.util.Date;
-
-/**
- * 用于测试, 一般注释掉
- * @author LHJ
- * @date 2021/7/5
- */
-@Data
-public class Student {
-
-    /** 学号 */
-    private long id;
-
-    private String username;
-
-    private int sex;
-
-    private int age;
-
-    /** 入学时间 */
-    private Date admissionDate;
-
-}
+//package com.neko.entity;
+//
+//import lombok.Data;
+//
+//import java.util.Date;
+//
+///**
+// * 测试, 打包需要注释掉
+// * @author LHJ
+// * @date 2021/7/5
+// */
+//@Data
+//public class Student {
+//
+//    /** 学号 */
+//    private long id;
+//
+//    private String username;
+//
+//    private int sex;
+//
+//    private int age;
+//
+//    /** 入学时间 */
+//    private Date admissionDate;
+//
+//}
--- src/main/java/com/neko/entity/SystemUser.java
+++ src/main/java/com/neko/entity/SystemUser.java
@@ -1,46 +1,46 @@
-package com.neko.entity;
-
-import com.neko.forward.annotation.Column;
-import com.neko.forward.annotation.Table;
-import lombok.Data;
-
-import java.util.Date;
-
-/**
- * 测试, 注释掉
- *
- * @title: User 表
- * @description: 测试 @Table + @Column
- * @author: SolarisNeko
- * @date: 2021/7/4
- */
-@Table(engine = "MyISAM", charset = "utf8mb4")
-@Data
-public class SystemUser {
-    /**
-     * PK, 用于 Index 快速搜索
-     * */
-    @Column(PK = true, autoIncrement = true)
-    private long userId;
-
-    /**
-     * 账号相关
-     * */
-    @Column(type = "varchar(255)", notNull = true)
-    private String userName;
-
-    @Column(notNull = true)
-    private String password;
-
-    /**
-     * 账号设置
-     * */
-    @Column
-    private String nickName;
-
-    @Column(type = "int")
-    private int age;
-
-    private Date createDate;
-
-}
+//package com.neko.entity;
+//
+//import com.neko.forward.annotation.Column;
+//import com.neko.forward.annotation.Table;
+//import lombok.Data;
+//
+//import java.util.Date;
+//
+///**
+// * 测试, 打包需要注释掉
+// *
+// * @title: User 表
+// * @description: 测试 @Table + @Column
+// * @author: SolarisNeko
+// * @date: 2021/7/4
+// */
+//@Table(engine = "MyISAM", charset = "utf8mb4")
+//@Data
+//public class SystemUser {
+//    /**
+//     * PK, 用于 Index 快速搜索
+//     * */
+//    @Column(PK = true, autoIncrement = true)
+//    private long userId;
+//
+//    /**
+//     * 账号相关
+//     * */
+//    @Column(type = "varchar(255)", notNull = true)
+//    private String userName;
+//
+//    @Column(notNull = true)
+//    private String password;
+//
+//    /**
+//     * 账号设置
+//     * */
+//    @Column
+//    private String nickName;
+//
+//    @Column(type = "int")
+//    private int age;
+//
+//    private Date createDate;
+//
+//}
